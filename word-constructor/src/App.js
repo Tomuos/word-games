5a8c2c5519f3c75942e76a2740149e1b3a720121
Tom Burns <Tomulvlup@gmail.com>
2023-08-15 22:57:12 +0100

    Missing export added to app

---
+++ word-constructor/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GameBoard from './GameBoard';
+import LetterPool from './LetterPool';
+import ControlPanel from './ControlPanel';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+function App() {
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="app">
+                <ControlPanel />
+                <GameBoard />
+                <LetterPool />
+            </div>
+        </DndProvider>
+    );
+}
+
+export default App;
