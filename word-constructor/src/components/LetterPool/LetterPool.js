2922765713a3324f95663379cab28875468bb1f5
Tom Burns <Tomulvlup@gmail.com>
2023-08-17 00:49:32 +0100

    Getting empty letter slots to appear

--- word-constructor/src/components/LetterPool/LetterPool.js
+++ word-constructor/src/components/LetterPool/LetterPool.js
@@ -4,12 +4,15 @@ import './LetterPool.css';
 // LETTER POOL COMPONENT should contain all the letters of the alphabet
 function LetterPool() {
     return (
-        <div className="letter-pool">
-            { "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter, index) => {
-                return <div key={index} className="letter">{letter}</div>
-            })}
+        <div className="letter-pool-container">
+            <div className="letter-pool">
+                { "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter, index) => {
+                    return <div key={index} className="letter">{letter}</div>
+                })}
+            </div>
         </div>
     );
 }
+
 
 export default LetterPool;
