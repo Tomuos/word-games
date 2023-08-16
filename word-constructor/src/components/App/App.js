751e42fa2bd9e1e4ce30e60314692a3a70c9b8f7
Tom Burns <Tomulvlup@gmail.com>
2023-08-16 22:52:03 +0100

    Few more fixes but working on emply letter slots

--- word-constructor/src/components/App/App.js
+++ word-constructor/src/components/App/App.js
@@ -6,39 +6,42 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import BoardSpot from "../BoardSpot/BoardSpot";
 
-
 function App() {
-  // I made a temporary word array to pass to the gameboard component
-  // to fix the vaguely worded error - item is undefined
-  const word = ["word", "word2", "word3", "word4"];
-  const [currentWord, setCurrentWord] = useState("APPLE"); // This is just an example word to start with.
-  const [showHint, setShowHint] = useState(false);
+    // I made a temporary word array to pass to the gameboard component
+    // to fix the vaguely worded error - item is undefined
+    const word = ["word", "word2", "word3", "word4"];
+    const [currentWord, setCurrentWord] = useState("APPLE"); // This is just an example word to start with.
+    const [showHint, setShowHint] = useState(false);
 
-
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="app">
-      {showHint && <div className="hint">Drag the letters to form the word!</div>}
-
-        <ControlPanel />
-        <div className="word-slots">
-    {Array.from(currentWord).map((_, index) => (
-        <BoardSpot key={index} onDropLetter={handleDropLetter} />
-    ))}
-</div>
-
-        <GameBoard word={word} />
-        <LetterPool />
-      </div>
-    </DndProvider>
-  );
-  function playAudio() {
-    const audio = new Audio("/path_to_assets/apple.mp3"); // Replace with the actual path to your audio file.
-    audio.play();
-    audio.onended = () => {
-        setShowHint(true);
+    const handleDropLetter = (letter) => {
+        // Handle what happens when a letter is dropped.
+        // For instance, check if the word is being formed correctly.
+        // This is a placeholder and should be expanded based on the game's requirements.
     };
+
+    const playAudio = () => {
+        const audio = new Audio("/path_to_assets/apple.mp3"); // Replace with the actual path to your audio file.
+        audio.play();
+        audio.onended = () => {
+            setShowHint(true);
+        };
+    };
+
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="app">
+                {showHint && <div className="hint">Drag the letters to form the word!</div>}
+                <ControlPanel />
+                <div className="word-slots">
+                    {Array.from(currentWord).map((_, index) => (
+                        <BoardSpot key={index} onDropLetter={handleDropLetter} />
+                    ))}
+                </div>
+                <GameBoard word={word} />
+                <LetterPool />
+            </div>
+        </DndProvider>
+    );
 }
 
-}
 export default App;
