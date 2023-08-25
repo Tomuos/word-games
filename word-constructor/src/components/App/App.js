a54de09895f3c5668069cfe84df454ebb99fae29
Tom Burns <Tomulvlup@gmail.com>
2023-08-25 23:21:00 +0100

    style changes, removed random selection

--- word-constructor/src/components/App/App.js
+++ word-constructor/src/components/App/App.js
@@ -1,117 +1,78 @@
 import React, { useState, useRef, useEffect, useCallback } from "react";
-
 import "./App.css";
-//import GameBoard from "../GameBoard/GameBoard";
 import LetterPool from "../LetterPool/LetterPool";
 import ControlPanel from "../ControlPanel/ControlPanel";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import BoardSpot from "../BoardSpot/BoardSpot";
-//import appleImage from "../../assets/images/fruit/apple.png";
 import appleAudio from "../../assets/audio/Apple.m4a";
 
-import "font-awesome/css/font-awesome.min.css";
-
 function App() {
-  const words = [
-    "apple",
-    "banana",
-    "blackberry",
-    "blueberry",
-    "cherry",
-    "lemon",
-    "mango",
-    "orange",
-    "pineapple",
-    "plum",
-    "raspberry",
-    "strawberry",
-  ];
+  const words = ["apple", "banana", "blackberry", "blueberry", "cherry", "lemon", "mango", "orange", "pineapple", "plum", "raspberry", "strawberry", "pear", "grapes", "kiwi"];
   const [currentWord, setCurrentWord] = useState("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0); // New state variable
   const [showHint, setShowHint] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
   const audioRef = useRef(new Audio(appleAudio));
   const [correctCount, setCorrectCount] = useState(0);
+  const [placedLetters, setPlacedLetters] = useState(Array(currentWord.length).fill(null));
+  const [resetSlots, setResetSlots] = useState(false);
 
+  useEffect(() => {
+    selectNextWord(); // Call the function when the component mounts
+  }, []);
 
   const incrementCorrectCount = () => {
     if (isWordComplete()) {
       setCorrectCount(correctCount + 1);
     }
   };
-  
-  
 
   const isWordComplete = () => {
     return placedLetters.every((letter, index) => letter === currentWord[index]);
-  }
-  
-  const [placedLetters, setPlacedLetters] = useState(
-    Array(currentWord.length).fill(null)
-  );
+  };
+
+  const selectNextWord = () => {
+    incrementCorrectCount();
+
+    if (currentWordIndex >= words.length - 1) {
+      setCurrentWordIndex(0); // Loop back to the beginning
+    } else {
+      setCurrentWordIndex(currentWordIndex + 1); // Increment the index
+    }
+
+    const nextWord = words[currentWordIndex].toUpperCase();
+    setCurrentWord(nextWord);
+    setPlacedLetters(Array(nextWord.length).fill(null));
+    setResetSlots(!resetSlots);
+  };
+
+  const handleDropLetter = useCallback((letter, index) => {
+    const newPlacedLetters = [...placedLetters];
+    newPlacedLetters[index] = letter;
+    setPlacedLetters(newPlacedLetters);
+  }, [placedLetters]);
+
+  const handleKeyPress = useCallback((event) => {
+    const key = event.key.toUpperCase();
+    if (currentWord.includes(key) && key.length === 1 && key.match(/[A-Z]/)) {
+      const index = placedLetters.findIndex(item => item === null);
+      if (index !== -1) {
+        handleDropLetter(key, index);
+      }
+    }
+  }, [placedLetters, currentWord, handleDropLetter]);
 
   useEffect(() => {
-    selectRandomWord(); // Call selectRandomWord once when the component mounts
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  
-  
-  const [resetSlots, setResetSlots] = useState(false); // Add this state
-
-  const selectRandomWord = () => {
-    incrementCorrectCount(); // Increment the count if the word is complete
-  
-    console.log('Before selecting a new word:', currentWord, placedLetters);
-    const remainingWords = words.filter(word => word.toUpperCase() !== currentWord);
-    const randomIndex = Math.floor(Math.random() * remainingWords.length);
-    const randomWord = remainingWords[randomIndex].toUpperCase();
-    
-    setCurrentWord(randomWord);
-    setPlacedLetters(Array(randomWord.length).fill(null));
-    setResetSlots(!resetSlots); // Toggle the resetSlots value
-  
-    console.log('After selecting a new word:', currentWord, placedLetters);
-  };
-  
-
-  // Define handleDropLetter first
-const handleDropLetter = useCallback((letter, index) => {
-  const newPlacedLetters = [...placedLetters];
-  newPlacedLetters[index] = letter;
-  setPlacedLetters(newPlacedLetters);
-}, [placedLetters]);
-
-// Then define handleKeyPress
-const handleKeyPress = useCallback((event) => {
-  const key = event.key.toUpperCase();
-  // ... rest of the code
-  if (currentWord.includes(key) && key.length === 1 && key.match(/[A-Z]/)) {
-    const index = placedLetters.findIndex(item => item === null);
-    if (index !== -1) {
-      handleDropLetter(key, index); // No problem here now
-    }
-  }
-  // ... rest of the code
-}, [placedLetters, currentWord, handleDropLetter]); // Dependencies are in correct order
-
-
-    useEffect(() => {
-      window.addEventListener('keydown', handleKeyPress);
-      return () => {
-        window.removeEventListener('keydown', handleKeyPress);
-      };
-    }, [handleKeyPress]);
-  
-  
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
   const isCorrect = (index) => {
     return placedLetters[index] === currentWord[index];
   };
-
-
-  
-  
-  
 
   const playAudio = () => {
     audioRef.current.play();
@@ -131,44 +92,25 @@ const handleKeyPress = useCallback((event) => {
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="app">
-      <div className="correct-header-container">
-        {isWordComplete() && <h1 className="correct-header">CORRECT!</h1>}
-      </div>
-      
-        {showHint && (
-          <div className="hint">Drag the letters to form the word!</div>
-        )}
-        <ControlPanel
-        
-          playAudio={playAudio}
-          isMuted={isMuted}
-          toggleMute={toggleMute}
-          
-        />
+        <div className="correct-header-container">
+          {isWordComplete() && <h1 className="correct-header">CORRECT!</h1>}
+        </div>
+        {showHint && <div className="hint">Drag the letters to form the word!</div>}
+        <ControlPanel playAudio={playAudio} isMuted={isMuted} toggleMute={toggleMute} />
         <div className="word-slots">
-  <p className="count-display">{correctCount}/{words.length}</p>
-  {placedLetters.map((letter, index) => (
-    <BoardSpot
-      key={index}
-      letter={letter} // Pass the letter from the placedLetters array
-      correct={isCorrect(index)}
-      onDropLetter={(droppedLetter) => handleDropLetter(droppedLetter, index)}
-      reset={resetSlots}
-    />
-  ))}
-  <button className="next-button" onClick={selectRandomWord}>Next</button>
-</div>
-
-        {currentWord && (
-          // images need to be in public folder to be accessible without importing
-          <img
-            src={`/images/fruit/${currentWord.toLowerCase()}.png`}
-            alt={currentWord.trim()}
-            className="word-image"
-          />
-        )}
-
-        {/* <GameBoard word={word} /> */}
+          <p className="count-display">{correctCount}/{words.length}</p>
+          {placedLetters.map((letter, index) => (
+            <BoardSpot
+              key={index}
+              letter={letter}
+              correct={isCorrect(index)}
+              onDropLetter={(droppedLetter) => handleDropLetter(droppedLetter, index)}
+              reset={resetSlots}
+            />
+          ))}
+          <button className="next-button" onClick={selectNextWord}>Next</button>
+        </div>
+        {currentWord && <img src={`/images/fruit/${currentWord.toLowerCase()}.png`} alt={currentWord.trim()} className="word-image" />}
         <LetterPool />
       </div>
     </DndProvider>
